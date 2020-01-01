3891b0d8af14d62c1d82cb5537871d13ed50fe7b
Kamlesh <kamlesh.sarki@gmail.com>
2019-12-31 22:35:40 -0800

    server.js

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+USE burgers_db;
+INSERT INTO burgers(burger_name,hamber)
+VALUES ('hamburger',false),('cheeseburger',false),('veggburger',false);
---
+++ db/shema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR (255) NOT NULL,
+    hamber BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
